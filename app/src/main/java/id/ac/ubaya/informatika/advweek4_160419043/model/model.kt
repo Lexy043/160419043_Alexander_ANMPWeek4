91c25b0209f186ea45a123da6a5ea5a8c472168a
Lexy043 <alexandercalvin43@gmail.com>
2022-03-09 22:48:24 +0700

    Week4Done

---
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4_160419043/model/model.kt
@@ -0,0 +1,9 @@
+package id.ac.ubaya.informatika.advweek4_160419043.model
+
+data class Student(
+    val id:String?,
+    val name:String?,
+    val dob:String?,
+    val phone:String?,
+    val photoUrl:String?
+)
---
+++ app/src/main/java/id/ac/ubaya/informatika/advweek4_160419043/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.ubaya.informatika.advweek4_160419043.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.ubaya.informatika.advweek4_160419043.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
